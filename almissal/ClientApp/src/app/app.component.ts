e559099b3ab4555d0c2457a1215eecf0bff8026d
Usama <usama.abbasi@ascertia.com>
2023-12-15 17:44:53 +0500

    whatsapp icon

--- almissal/ClientApp/src/app/app.component.ts
+++ almissal/ClientApp/src/app/app.component.ts
@@ -2,7 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: [
+    `
+      #whatsAppBtn {
+        display: block;
+        position: fixed;
+        bottom: 20px;
+        right: 30px;
+        z-index: 99;
+        font-size: 18px;
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'app';
